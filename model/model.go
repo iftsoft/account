50cc6a585ef72afc3e44c06fec3c3839e5264270
ivan <ivan@ift.com.ua>
2018-05-25 22:37:46 +0300

    Add model and store packages

---
+++ model/model.go
@@ -0,0 +1,31 @@
+package model
+
+import "time"
+
+// Enumeration of account state
+const (
+	State_Active    int = iota
+	State_Blocked
+)
+// Account object declaration
+type Account struct {
+	Name 		string		`json:"name"`		// Unique account ID number
+	State 		int			`json:"state"`		// State of account
+	Owner 		string		`json:"owner"`		// Name of account owner
+	Currency	string		`json:"currency"`	// Currency ISO code
+	Amount		float32		`json:"amount"`		// Amount of currency
+	Created		time.Time	`json:"created"`	// Account creation time
+	Updated		time.Time	`json:"updated"`	// Account last update time
+}
+// Account list declaration
+type AccountList []*Account
+
+// Interface to account storage
+type AccountKeeper interface {
+	InsertAccount(item *Account) error			// Put new account to the storage
+	UpdateAccount(item *Account) error			// Set new account data by account name
+	DeleteAccount(name string) error			// Remove account from the storage by name
+	GetAccount(name string) (*Account, error)	// Get account from the storage by name
+	GetAccountList() AccountList				// Get all accounts from the storage
+}
+
---
+++ store/storage.go
@@ -0,0 +1,107 @@
+package store
+
+import (
+	"sync"
+	"account/model"
+	"errors"
+)
+
+// Error strings
+const (
+	errAccountIsExist	= "Account is already exist"
+	errAccountNotExist	= "Account is not exist"
+)
+
+// Return interface to account storage
+func GetAccountKeeper() model.AccountKeeper {
+	return &storage
+}
+
+// Singleton instance of account storage
+var storage = accountStorage { items: make(map[string]*model.Account), }
+
+// Account storage definition
+type accountStorage struct {
+	sync.RWMutex	// For access synchronization
+	items		map[string]*model.Account	// Account name is a map key
+}
+
+// Put new account to the storage
+func (this *accountStorage)InsertAccount(item *model.Account) error {
+	this.Lock()		// Lock storage for write
+	defer this.Unlock()
+
+	// Check storage for account name
+	_, found := this.items[item.Name]
+	if found {
+		// Account redefinition error
+		return errors.New(errAccountIsExist)
+	}
+	// Put account into map by name
+	this.items[item.Name] = item
+	return nil
+}
+
+// Set new account data by account name
+func (this *accountStorage)UpdateAccount(item *model.Account) error {
+	this.Lock()		// Lock storage for write
+	defer this.Unlock()
+
+	// Check storage for account name
+	_, found := this.items[item.Name]
+	if !found {
+		// Account is not found error
+		return errors.New(errAccountNotExist)
+	}
+	// Replace map entry with new account data
+	this.items[item.Name] = item
+	return nil
+}
+
+// Remove account from the storage by name
+func (this *accountStorage)DeleteAccount(name string) error {
+	this.Lock()		// Lock storage for write
+	defer this.Unlock()
+
+	// Check storage for account name
+	_, found := this.items[name]
+	if !found {
+		// Account is not found error
+		return errors.New(errAccountNotExist)
+	}
+	// Delete map entry
+	delete(this.items, name)
+	return nil
+}
+
+// Get account from the storage by name
+func (this *accountStorage)GetAccount(name string) (item *model.Account, err error) {
+	this.RLock()		// Lock storage for read
+	defer this.RUnlock()
+
+	// Check storage for account name
+	item, found := this.items[name]
+	if !found {
+		// Account is not found error
+		return nil, errors.New(errAccountNotExist)
+	}
+	// Return map entry
+	return item, nil
+}
+
+// Get all accounts from the storage
+func (this *accountStorage)GetAccountList() model.AccountList {
+	this.RLock()		// Lock storage for read
+	defer this.RUnlock()
+
+	// Init empty account list
+	list := make(model.AccountList, 0)
+	// Iterate through all map entries
+	for _, item := range this.items {
+		// Add account to list
+		list = append(list, item)
+	}
+	return list
+}
+
+
